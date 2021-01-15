141613a1ed9f7272fb4ff4aceade959ec7288fc1
prabanjanraja <prabanjan.m@outlook.com>
2021-01-15 19:14:59 +0530

    calling random2 fun for every attribute

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const form =
+{
+    "ActionDispalyName": "Login",
+    "Attributes": [
+        {
+            "Name": "Username",
+            "Size": "10",
+            "Type": "TextBox"
+        },
+        {
+            "Name": "Password",
+            "Size": "10",
+            "Type": "SecretTextBox"
+        }
+    ]
+}
